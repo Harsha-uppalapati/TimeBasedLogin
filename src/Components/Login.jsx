ecd9bdd8b478f267a4596b3dfed7b5f7bc71f686
kiransai <kiransai241998@gmail.com>
2022-12-05 09:41:26 +0530

    first

---
+++ src/Components/Login.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Login.css";
+const Login = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="screen">
+          <div className="screen__content">
+            <form className="login">
+              <div className="login__field">
+                <i className="login__icon fas fa-user" />
+                <input
+                  type="text"
+                  className="login__input"
+                  placeholder="User name / Email"
+                />
+              </div>
+              <div style={{ color: "red", fontSize: "14px" }}>err</div>
+              <div className="login__field">
+                <i className="login__icon fas fa-lock" />
+                <input
+                  type="password"
+                  className="login__input"
+                  placeholder="Password"
+                />
+              </div>
+              <div style={{ color: "red", fontSize: "14px" }}>err</div>
+              <button className="button login__submit">
+                <span className="button__text">Log In Now</span>
+                <i className="button__icon fas fa-chevron-right" />
+              </button>
+            </form>
+          </div>
+          <div className="screen__background">
+            <span className="screen__background__shape screen__background__shape4" />
+            <span className="screen__background__shape screen__background__shape3" />
+            <span className="screen__background__shape screen__background__shape2" />
+            <span className="screen__background__shape screen__background__shape1" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
